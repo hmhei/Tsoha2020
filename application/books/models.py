e939e69ac5e9fe0c1065bee22458c8c9fa938d03
hmhei <hmhei@outlook.com>
2020-02-23 10:38:51 +0200

    Muokkauksia tietokantatauluihin

--- application/books/models.py
+++ application/books/models.py
@@ -4,7 +4,7 @@ from application.authors.models import Author
 
 from sqlalchemy.sql import text
 
-bookAuthors = db.Table('book_authors',
+bookAuthor = db.Table('book_author',
     db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
     db.Column('author_id', db.Integer, db.ForeignKey('author.id'))
 )
@@ -13,7 +13,7 @@ class Book(Base):
 
     name = db.Column(db.String(144), nullable=False)
     author = db.relationship(
-        "Author", secondary=bookAuthors, backref='book_authors', lazy=True)
+        "Author", secondary=bookAuthor, backref='book_author', lazy=True, cascade="delete")
     published = db.Column(db.Integer, nullable=False)
     count = db.Column(db.Integer, nullable=False)
     
@@ -26,14 +26,14 @@ class Book(Base):
     @staticmethod
     def list_authors(bookid):
         stmt = text("SELECT Author.name FROM author"
-                    " INNER JOIN book_authors ON book_authors.author_id = Author.id"
-                    " LEFT JOIN book ON Book.id = book_authors.book_id)"
+                    " INNER JOIN book_author ON book_author.author_id = Author.id"
+                    " LEFT JOIN book ON Book.id = book_author.book_id)"
                     " WHERE Book.id = :bookid"
                     " GROUP BY Book.id").params(bookid=bookid)
         res = db.engine.execute(stmt)
 
         response = []
         for row in res:
-            response.append({"authors": row[0]})
+            response.append({"author": row[0]})
 
         return response
