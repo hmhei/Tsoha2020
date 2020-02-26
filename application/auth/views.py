92fb539c781d769453067790eacf824bdc6fda7f
hmhei <hmhei@outlook.com>
2020-02-26 22:39:16 +0200

    Lisätty ylläpitäjälle käyttäjien muokkaus/poisto

--- application/auth/views.py
+++ application/auth/views.py
@@ -4,6 +4,7 @@ from flask_login import login_user, logout_user, current_user
 from application import app, db, login_required
 from application.auth.models import User
 from application.auth.forms import LoginForm, RegisterForm
+from application.loans.models import Loan
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -45,6 +46,7 @@ def auth_register():
   
         return redirect(url_for("loans_index"))
 
+#Käyttäjän omien tietojen muokkaus
 @app.route("/auth/modify/", methods = ["GET"])
 @login_required(role="USER")
 def auth_modify():
@@ -58,6 +60,7 @@ def auth_modify():
 
     return render_template("auth/modifyform.html", form=form)
 
+#Käyttäjän omien tietojen tallennus
 @app.route("/auth/modify/", methods = ["POST"])
 @login_required(role="USER")
 def auth_save():
@@ -78,3 +81,62 @@ def auth_save():
     db.session().commit()
 
     return redirect(url_for("index"))
+
+#ADMIN: Käyttäjien listaus
+@app.route("/auth/list/", methods = ["GET"])
+@login_required(role="ADMIN")
+def auth_index():
+    return render_template("auth/list.html",
+    users = User.query.all())
+
+#ADMIN: Käyttäjän poistaminen
+@app.route("/auth/list/delete/<user_id>", methods = ["GET"])
+@login_required(role="ADMIN")
+def auth_delete(user_id):
+
+    user = User.query.get(user_id)
+    loancount = Loan.query.filter_by(account_id = user_id).count()
+
+    #Admin-tarkistus ei tässä tapauksessa ole välttämätön, mutta varmuuden vuoksi
+    if user.admin == False and loancount == 0:
+        db.session().delete(user)
+        db.session().commit()
+
+    return redirect(url_for("auth_index"))
+
+#ADMIN: Käyttäjän tiedot / tietojen muokkaaminen
+@app.route("/auth/list/info/<user_id>", methods = ["GET"])
+@login_required(role="ADMIN")
+def auth_info(user_id):
+
+    user = User.query.get(user_id)
+    form = RegisterForm()
+
+    form.name.data = user.name
+    form.username.data = user.username
+    form.address.data = user.address
+    form.phone.data = user.phone
+
+    return render_template("auth/infoform.html", form = form, num = user_id)
+
+@app.route("/auth/list/info/<user_id>", methods = ["POST"])
+@login_required(role="ADMIN")
+def auth_adminsave(user_id):
+
+    form = RegisterForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/infoform.html", form = form, num = user_id)
+
+    user = User.query.get(user_id)
+
+    user.name = form.name.data
+    user.username = form.username.data
+    user.password = form.password.data
+    user.address = form.address.data
+    user.phone = form.phone.data
+
+    db.session().commit()
+
+    return redirect(url_for("auth_index"))
+
